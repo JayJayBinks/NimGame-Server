c08e1b094d17a403fcf1d96e9cb98decf361876b
julian <julian.wrastil@cognizant.com>
2018-02-26 11:05:29 +0100

    added todo for writing integration tests

--- src/main/java/github/jjbinks/nimgame/api/NimGameApiController.java
+++ src/main/java/github/jjbinks/nimgame/api/NimGameApiController.java
@@ -27,7 +27,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-//TODO write Spring Boot Tests
+//TODO write Spring Boot Tests (how many as they are expensive?)
 public class NimGameApiController implements NimGameApi {
     public static final Logger LOGGER = LoggerFactory.getLogger(NimGameApiController.class);
 
